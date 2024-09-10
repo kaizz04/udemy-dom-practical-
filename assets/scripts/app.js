aaa8a48356261fd8da3d038b97287629fa18e133
kaizz04 <kaveesh@marketpushapps.com>
2024-09-10 14:54:28 +0530

    Deleting Movie elements

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -18,7 +18,21 @@ const updateUI = () =>{
 
 };
 
-const renderNewMovieElement = (title,imageUrl,rating) =>{
+const deleteMovieHandler = (movieId)=>{
+    let movieIndex=0;
+    for(const movie of movies){
+        if(movie.id === movieId){
+          break;
+        }
+        movieIndex++;
+    }
+    movies.splice(movieIndex,1);
+    const listRoot = document.getElementById('movie-list');
+    listRoot.children[movieIndex].remove();
+    //listRoot.removeChild(listRoot.children[movieIndex]);
+};
+
+const renderNewMovieElement = (id,title,imageUrl,rating) =>{
     const newMovieElement = document.createElement('li');
     newMovieElement.className = 'movie-element';
     newMovieElement.innerHTML = `
@@ -30,6 +44,7 @@ const renderNewMovieElement = (title,imageUrl,rating) =>{
         <p>${rating}/5 stars</p>
     </div>
     `;
+    newMovieElement.addEventListener('click',deleteMovieHandler.bind(null,id));
     const listRoot = document.getElementById('movie-list');
     listRoot.append(newMovieElement);
 }
@@ -73,6 +88,7 @@ const addMovieHandler = ()=>{
         return;
     }
     const newMovie = {
+        id:Math.random().toString(),
         title:titleValue,
         image:imageUrlValue,
         rating:ratingValue
@@ -81,7 +97,7 @@ const addMovieHandler = ()=>{
     movies.push(newMovie);
     console.log(movies);
     toggleMovieModel();
-    renderNewMovieElement(newMovie.title,newMovie.image,newMovie.rating);
+    renderNewMovieElement(newMovie.id,newMovie.title,newMovie.image,newMovie.rating);
     updateUI();
 
 };
